77a37f850e263aa8cbb08faadef8da6a45a2f257
David Marr <david.marr@contextlabs.com>
2025-01-18 21:54:49 -0500

    Add some goodies

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,19 +3,25 @@ const colorMode = useColorMode()
 </script>
 
 <template>
-  <div>
+  <div class="container mx-auto">
     <p class="p-4 pb-2">
-      <select
-        v-model="colorMode.preference"
-        class="border w-24 h-8 dark:bg-gray-900 dark:text-white dark:border-gray-700"
-      >
-        <option value="system">System</option>
-        <option value="light">Light</option>
-        <option value="dark">Dark</option>
-      </select>
+      <Select v-model="colorMode.preference">
+        <SelectTrigger  class="w-40">
+          <SelectValue placeholder="Color mode" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="system">System</SelectItem>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+        </SelectContent>
+      </Select>
     </p>
     <div class="mx-auto p-4">
-      <Alert>Foo</Alert>
+      <Alert>
+        <AlertDescription>
+          This is a demo of a Nuxt.js app with Tailwind CSS and Radix UI components.
+        </AlertDescription>
+      </Alert>
       <slot />
     </div>
   </div>
