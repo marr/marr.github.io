74cab3f17fdd8b2bc864813cc6931a606ffff77c
David Marr <david.marr@contextlabs.com>
2025-01-19 10:54:14 -0500

    Try using pnpm with workflows

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -26,9 +26,3 @@ const colorMode = useColorMode()
     </div>
   </div>
 </template>
-
-<style lang="postcss">
-body {
-  @apply min-h-screen bg-white dark:bg-gray-800 dark:text-gray-200;
-}
-</style>
