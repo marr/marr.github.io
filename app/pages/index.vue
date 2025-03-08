da048139c621ac6dc5a3e3c3bbcd337457eeba3a
David Marr <david.marr@contextlabs.com>
2025-03-08 00:04:26 -0500

    Labsin

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col h-72 max-w-[950px] items-center justify-center">
     <LiquidLogo class="translate-x-5" image-url="/cxl-icon.svg" />
     <ClientOnly>
-      <MorphingText :texts="['Context', 'Labs']" />
+      <MorphingText :texts="['Labs', 'Context']" />
     </ClientOnly>
   </div>
 </template>
