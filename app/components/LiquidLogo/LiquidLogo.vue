c06cf7d6b65f308a8af7cf1b6e5e734fe63675aa
David Marr <david.marr@contextlabs.com>
2025-07-07 16:50:00 -0400

    upgrades

--- app/components/LiquidLogo/LiquidLogo.vue
+++ app/components/LiquidLogo/LiquidLogo.vue
@@ -3,7 +3,7 @@
     v-if="processing"
     class="flex size-full items-center justify-center text-2xl font-bold text-primary/50"
   >
-    <span> Context...  </span>
+    <span> loading...  </span>
   </div>
   <canvas
     ref="liquidLogoRef"
