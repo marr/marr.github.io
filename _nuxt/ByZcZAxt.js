4e82c5b062eeeaa75a695ba7cd0984737298b4ba
marr <44376+marr@users.noreply.github.com>
2026-01-19 23:10:23 +0000

    Deploying to gh-pages from @ marr/marr.github.io@49db397b9d07c24eb08a711faa77a11ada0b19f6 🚀

--- _nuxt/ByZcZAxt.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./BEiQZamu.js";import a from"./Dcaf3JBS.js";import{x as e,M as n,e as s,w as c,N as i}from"./Cx-P0jSD.js";import"./BN_7HF1G.js";const d={__name:"ProseCaution",setup(p){const o=e();return(r,f)=>(s(),n(a,{color:"error",icon:i(o).ui.icons.caution},{default:c(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
