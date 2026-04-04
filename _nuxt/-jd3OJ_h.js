ba1712a839df707205f674d9fd2624634dec56ce
marr <44376+marr@users.noreply.github.com>
2026-04-04 18:17:58 +0000

    Deploying to gh-pages from @ marr/marr.github.io@df67741d945485fde4230b5845106cc62ca2c1a7 🚀

--- _nuxt/-jd3OJ_h.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./B2G12K-c.js";import e from"./SO2dAOPT.js";import{H as n,h as s,M as a,w as i,N as p}from"./CdFTdG92.js";import"./BN_7HF1G.js";const d={__name:"ProseNote",setup(c){const o=n();return(r,f)=>(s(),a(e,{color:"info",icon:p(o).ui.icons.info},{default:i(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
