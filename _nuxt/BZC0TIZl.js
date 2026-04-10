f500bc2de12a22537bccff6dc1e084e6e0cf3fea
marr <44376+marr@users.noreply.github.com>
2026-04-10 14:43:16 +0000

    Deploying to gh-pages from @ marr/marr.github.io@267f8e89b742f53b5a1d64559fa72fc675f9f615 🚀

---
+++ _nuxt/BZC0TIZl.js
@@ -0,0 +1,1 @@
+import{r as n}from"./AEvc3w6A.js";import a from"./a3No22sz.js";import{H as s,h as t,M as e,w as i,N as p}from"./BUgARXIr.js";import"./BN_7HF1G.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(t(),e(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
