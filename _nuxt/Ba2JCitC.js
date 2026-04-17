1d7fdc5daed15e0812d78ecbf1144580df8c0e4c
marr <44376+marr@users.noreply.github.com>
2026-04-17 12:44:33 +0000

    Deploying to gh-pages from @ marr/marr.github.io@aa9f8d7df4a4928a82e4263cb7d3826c02acf56f 🚀

--- _nuxt/Ba2JCitC.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./JxubUzKz.js";import t from"./B9W07WG9.js";import{H as e,h as a,M as p,w as c,N as n}from"./1F7PZ5GE.js";import"./BN_7HF1G.js";const d={__name:"ProseTip",setup(i){const o=e();return(r,f)=>(a(),p(t,{color:"success",icon:n(o).ui.icons.tip},{default:c(()=>[s(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
